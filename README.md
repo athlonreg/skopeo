d2f5d3109ae2f6d32b8874f139714fea5246ebfa
athlonreg <15563836030@163.com>
2024-12-17 20:34:00 +0800

    shell action and list

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# skopeo
+## sync images from huawei cloud to ghcr with skopeo
+
+sync all images of kubernetes v1.28.15 and kubesphere v4.1.2 to ghcr. it contains kubernetes, and all extensions of kubesphere.
+
+all images is generate by [kubesphere get images tools](https://get-images.kubesphere.io/), the images list can found in [images-list-mirrors-kubernetes-v1.28.15-kubesphere-v4.1.2.txt](images-list-mirrors-kubernetes-v1.28.15-kubesphere-v4.1.2.txt).
---
+++ skopeo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#set -ex
+
+KUBERNETES_VERSION=${KUBERNETES_VERSION:-v1.28.15}
+KUBESPHERE_VERSION=${KUBESPHERE_VERSION:-v4.1.2}
+IMAGES_LIST_FILE=${IMAGES_LIST_FILE:-images-list-mirrors-kubernetes-${KUBERNETES_VERSION}-kubesphere-${KUBESPHERE_VERSION}.txt}
+
+REGISTRY=${REGISTRY:-ghcr.io}
+REPOSITORY=${REPOSITORY:-library}
+GITHUB_TOKEN=${GITHUB_TOKEN:-token}
+
+for GROUP in $(cat ${IMAGES_LIST_FILE} | grep "##" | awk '{print $2}' | grep -vE "KubeSphere|Kubernetes")
+do
+	echo "start syncing image in group $GROUP"
+	echo
+  for IMAGE in $(cat ${IMAGES_LIST_FILE} | grep $GROUP -A 1000 | grep -v "## $GROUP" | sed '/##/,$d')
+	do
+		echo "syncing $IMAGE..."
+		IMAGE_NAME=$(echo "$IMAGE" | awk -F '/' '{print $3}' | awk -F ':' '{print $1}')
+		IMAGE_TAG=$(echo "$IMAGE" | awk -F '/' '{print $3}' | awk -F ':' '{print $2}')
+		SRC=docker://$IMAGE
+		DEST=docker://$REGISTRY/$REPOSITORY/$IMAGE_NAME:$IMAGE_TAG
+		echo
+		sleep 2
+		if skopeo copy --dest-creds $QUAYIO_TOKEN --multi-arch all --override-os linux --dest-creds $GITHUB_TOKEN ${SRC} ${DEST}
+		then
+		  echo "Process: sync $SRC to $DEST successfully"
+		else
+		  echo "$SRC sync failed"
+		  exit 1
+		fi
+		sleep 1
+	done
+	echo "image in group $GROUP has synced successfully"
+	sleep 2
+done
