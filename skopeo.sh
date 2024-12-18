4c48843e08b213eba499a7bd056ecbcdec108c71
athlonreg <15563836030@163.com>
2024-12-18 09:56:28 +0800

    added quay

--- skopeo.sh
+++ skopeo.sh
@@ -6,9 +6,23 @@ KUBERNETES_VERSION=${KUBERNETES_VERSION:-v1.28.15}
 KUBESPHERE_VERSION=${KUBESPHERE_VERSION:-v4.1.2}
 IMAGES_LIST_FILE=${IMAGES_LIST_FILE:-images-list-mirrors-kubernetes-${KUBERNETES_VERSION}-kubesphere-${KUBESPHERE_VERSION}.txt}
 
-REGISTRY=${REGISTRY:-ghcr.io}
-REPOSITORY=${REPOSITORY:-athlonreg}
-GITHUB_TOKEN=${GITHUB_TOKEN:-token}
+REPO=${REPO:-GHCR}
+
+GHCR_REGISTRY=${GHCR_REGISTRY:-ghcr.io}
+QUAY_REGISTRY=${QUAY_REGISTRY:-quay.io}
+REGISTRY=${GHCR_REGISTRY}
+GHCR_REPOSITORY=${GHCR_REPOSITORY:-athlonreg}
+QUAY_REPOSITORY=${QUAY_REPOSITORY:-taolu}
+REPOSITORY=${GHCR_REPOSITORY}
+GHCR_TOKEN=${GHCR_TOKEN:-token}
+QUAY_TOKEN=${QUAY_TOKEN:-token}
+TOKEN=${GHCR_TOKEN}
+
+if [ "$REPO" == "QUAY" ]; then
+  REGISTRY=${QUAY_REGISTRY}
+  REPOSITORY=${QUAY_REPOSITORY}
+  TOKEN=${QUAY_TOKEN}
+fi
 
 for GROUP in $(cat ${IMAGES_LIST_FILE} | grep "##" | awk '{print $2}' | grep -vE "KubeSphere|Kubernetes")
 do
@@ -23,7 +37,7 @@ do
 		DEST=docker://$REGISTRY/$REPOSITORY/$IMAGE_NAME:$IMAGE_TAG
 		echo
 		sleep 2
-		if skopeo copy --src-creds $HUAWEI_TOKEN --dest-creds $GITHUB_TOKEN --multi-arch all --override-os linux ${SRC} ${DEST}
+		if skopeo copy --src-creds $HUAWEI_TOKEN --dest-creds $TOKEN --multi-arch all --override-os linux ${SRC} ${DEST}
 		then
 		  echo "Process: sync $SRC to $DEST successfully"
 		else
